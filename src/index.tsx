49a66ccabbc9903d3ac6d93b4a227459f8755196
jooooonis <xowns3213@naver.com>
2023-06-20 21:18:41 +0900

    🚀 feat : init

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import './tailwind.css';
+import './mvp.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
