8f9b2f9900f1f358bd45341d841bbdcd1f6012cd
jooooonis <xowns3213@naver.com>
2023-06-20 23:03:02 +0900

    🚧 chore : color configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fff7ec',
+        secondary: '#f5e3d9',
+        system: {
+          black: '#32312d',
+          white: '#FFFFFF',
+          gray: '#F5F5F5',
+        },
+      },
+    },
   },
   plugins: [],
 };
